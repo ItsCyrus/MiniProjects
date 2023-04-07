fc438fd951c4064a8ebf06fd120b647dea62ce4e
Cyrus Bhandari <65149993+Cyrus-GHub@users.noreply.github.com>
2023-04-07 10:42:05 +0530

    api fleshed out

--- API$/index.js
+++ API$/index.js
@@ -1,11 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/api', (req, res) => {
-    const apiKey =req.query.apiKey;
-    // Validation pending
-    //Billing pending
-    res.send({data: 'API Key received'})
+const stripe = require("stripe")(
+  "sk_test_51MtoXmSIgwV9JjGwLEGjUwXZShVUqzPbwFU6Jub22wwvUXy6ZYtITkOFGUI95bnlDnVxuf7ISfF0rmhTvogwWlb300PmQahg7c"
+);
+
+app.get("/api", (req, res) => {
+  const apiKey = req.query.apiKey;
+  // Validation pending
+  //Billing pending
+  res.send({ data: "API Key received" });
+});
+
+app.post("/checkout", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    mode: "subscription",
+    payment_method_type: ["card"],
+    line_items: [
+      {
+        price: "price_YOUR-PRICE",
+      },
+    ],
+    success_url:
+      "http://localhost:5000/success?session_id={CHECKOUT_SESSION_ID}",
+    cancel_url: "http://localhost:5000/error",
+  });
+
+  res.send(session);
 });
 
 app.listen(8080);
